e61bed75798cfe303ec7114e48a43a0726fb6178
Sasha Azad <sazad6@gatech.edu>
2015-09-27 03:38:54 -0400

    All keys from the table

--- MW_Scraper.py
+++ MW_Scraper.py
@@ -5,17 +5,26 @@ import json
 
 # The Request
 
-
-# page = requests.get('http://marvel.wikia.com/api.php?format=json&page=Thor_(Thor_Odinson)&action=parse');
-
 page = requests.get('http://marvel.wikia.com/wiki/Thor_(Thor_Odinson)')
 tree = html.fromstring(page.text)
 
 infoTable = tree.xpath("//table[@class='infobox-table']/tr")
+
+charDict = {};
+
 print len(infoTable)
 
 for i in range(0,29):
 	value = tree.xpath("//table[@class='infobox-table']/tr[{i}]/td".format(i=i))
-	print "---------------"
-	for j in value:
-		print j.text_content().strip()
+	# print "---------------"
+	valueList = []
+	for j in range(1,len(value)):
+		valueList.append(value[j].text_content().encode('utf-8').strip())
+
+	try: 
+		charDict[value[0].text_content().encode('utf-8').strip()] = valueList
+	except: 
+		pass
+
+print charDict.keys()
+	
