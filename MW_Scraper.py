7221fce0fdc11e24f515ac132bab6742f8a7b53a
Sasha Azad <sazad6@gatech.edu>
2015-10-07 17:45:46 -0400

    Pulling all individual character pages from Marvel Wikia

--- MW_Scraper.py
+++ MW_Scraper.py
@@ -4,33 +4,83 @@ import requests
 import json
 
 BASE_URL_FOR_WIKIA = 'http://marvel.wikia.com'
+FILE_FOR_CHARACTER_LINKS = "CharacterURLs.txt"
 
+
+'''
+	Completed : Methods to get every set of character page links from the Marvel Wikia
+'''
+
+def getAllCharacterLinks():
+	"Writing each pageinated link from the Marvel Wikia of Characters to a new line in the file assigned to FILE_FOR_CHARACTER_LINKS"
+
+	url = "http://marvel.wikia.com/wiki/Category:Characters?display=exhibition&sort=alphabetical&pageuntil=Abraham+Erskine+%28Earth-9047%29#mw-pages";
+	
+	'''
+		COMMENTED FOR FUTURE USE 
+		Until refresh of data needed. If refresh is needed, check next commented section below.
+	'''
+	# fileForCharacterURLs = open('CharacterURLs.txt', 'w')
+
+	while url != "Finis":
+		urlTemp = getNextPageOfCharacterLinks(url)
+		if urlTemp != "Finis":
+			url = BASE_URL_FOR_WIKIA + urlTemp
+			
+			'''
+				COMMENTED FOR FUTURE USE
+				Stored all the URLs to the File on Oct 7th.
+				Need to uncomment the next 2 file write lines only if the data needs to be refreshed. 
+			'''
+			# fileForCharacterURLs.write(url);
+			# fileForCharacterURLs.write("\n");
+
+
+		else:
+			url = "Finis";
 
 def getNextPageOfCharacterLinks(url):
 	"get the link of the next page of characters to go to"
 
 	nextPage = "";
-	page = requests.get('http://marvel.wikia.com/wiki/Category:Characters?display=exhibition&sort=alphabetical')
+	page = requests.get(url)
 	tree = html.fromstring(page.text)
 	infoTable = tree.xpath("//div[@id='mw-pages']/a")
 
 	for eachLink in infoTable:
 		if(eachLink.text_content() == "next 200"):
-			nextPage = infoTable[0].values()[0];
-			break	
+			nextPage = eachLink.values()[0]
+			return nextPage	
 
-	return
+	if nextPage == "":
+		return "Finis"
 
-def getCharacterNamesFromURL(url):
+
+'''
+	In Progress: Methods to get character data from character
+'''
+
+
+
+'''
+	sample URL to be supplied to this method: http://marvel.wikia.com/wiki/Category:Characters?display=exhibition&sort=alphabetical&pagefrom=Ahadi+%28Earth-9590%29#mw-pages
+'''
+
+def getIndividualCharacterPagesFromURL(url):
 	"build a list of all the characters with their Wiki names to encode into URLs"
 
 	charList = []
-	page = requests.get('http://marvel.wikia.com/wiki/Category:Characters?sort=alphabetical')
+	page = requests.get()
 	tree = html.fromstring(page.text)
-	infoTable = tree.xpath("//div[@class='mw-content-ltr']/table/tr")
+	infoTable = tree.xpath("//div[@class='mw-content-ltr']/table/tr/td/ul/li/a")
 
-	# text returned as a giant string, splitlines gives us each character indpendantly for the page.
-	charList = infoTable[1].text_content().strip().splitlines()
+	flag = False;
+	for each in infoTable:
+		if flag:
+			charList.append(each.values())
+		elif each.values()[0] == "/wiki/Category:Secret_Wars_participants":
+			flag = True;
+
 	return charList
 
 
@@ -62,6 +112,8 @@ def getTableDataGivenUrl(url):
 	return charDict
 
 
+
+# print getIndividualCharacterPagesFromURL()
 
 
 
